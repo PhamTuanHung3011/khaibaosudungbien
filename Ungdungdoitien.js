f2810ba938c23ba70181914e8a32e26ecfa7444e
unknown <phamtu571990@gmail.com>
2021-10-10 23:29:00 +0700

    bo xung bt doi tien

---
+++ Ungdungdoitien.js
@@ -0,0 +1,62 @@
+
+
+let value11;
+let value22;
+let button22;
+let sotien22;
+let tong22;
+ // vailue1 = document.getElementById('1');
+ // sotien1 = document.getElementById('sotien').value;
+
+function sotien222(){
+    sotien22=document.getElementById('sotien').value;
+    console.log(sotien22);
+}
+
+function onselectchange1(){
+   value11= document.getElementById('1').value;
+   switch (value11){
+       case '000': { console.log()}; break;
+       case '1000': { console.log()}; break;
+       case '23000':{console.log()}; break;
+       case '17000': {console.log()}; break;
+       case '21000': {console.log()}; break;
+       case '20000' : {console.log()}; break;
+       default:('Return again!');
+
+    console.log(value11);
+}
+function onselectchange2() {
+    value22 = document.getElementById('2').value;
+    switch (value22) {
+        case '1000': {
+            console.log()
+        }
+            ;
+            break;
+        case '23000': {
+            console.log()
+        }
+            ;
+            break;
+        case '17000': {
+            console.log()
+        }
+            ;
+            break;
+        case '21000': {
+            console.log()
+        }
+            ;
+            break;
+        case '20000' : {
+            console.log()
+        }
+            ;
+            break;
+        default: {
+            console.log('retun!')
+        }
+    }
+    console.log(value22);
+}
